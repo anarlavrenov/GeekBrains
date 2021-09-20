7e1ca724b97078464fcd6d6d5ee5f23139855917
anarlavrenov <90818739+anarlavrenov@users.noreply.github.com>
2021-09-20 08:07:51 +0300

    Delete lesson_1_introduction directory

--- lesson_1_introduction/home_task/home_task_1.py
+++
@@ -1,9 +0,0 @@
-a = 7
-b = 'Hello'
-print(a)
-print(b)
-
-c = int(input('Введите число '))
-d = input('Как тебя зовут? ')
-print(c)
-print(d)
--- lesson_1_introduction/home_task/home_task_3.py
+++
@@ -1,8 +0,0 @@
-n = (input('Введите число '))
-
-a = int(n + n)
-b = int(n + n + n)
-
-c = int(n)
-
-print(a + b + c)
